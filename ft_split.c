8fe7a616e791a851f6ed028bf50e09f9a0a6f6a7
paromero <paromeropons@hotmail.com>
2023-10-24 17:22:10 +0200

    arreglado split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 09:38:49 by paromero          #+#    #+#             */
-/*   Updated: 2023/10/23 11:28:32 by paromero         ###   ########.fr       */
+/*   Updated: 2023/10/24 11:44:45 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,12 +76,12 @@ void	free_mem(char **array)
 char	**ft_split(char const *s, char c)
 {
 	char	**array;
-	size_t	substr;
+	size_t	substrlen;
 
 	if (!s)
 		return (NULL);
-	substr = count_substr(s, c);
-	array = (char **)ft_calloc(substr + 1, sizeof(char *));
+	substrlen = count_substr(s, c);
+	array = (char **)ft_calloc(substrlen + 1, sizeof(char *));
 	if (!array)
 		return (NULL);
 	if (allocate_substr(array, s, c) == -1)
