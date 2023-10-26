f7b101a7e7f7c8e3123536e71d480a47912ca854
paromero <paromeropons@hotmail.com>
2023-10-26 16:02:30 +0200

    arreglado strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 10:08:08 by paromero          #+#    #+#             */
-/*   Updated: 2023/10/23 10:08:57 by paromero         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:36:45 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	strat;
+	size_t	start;
 	size_t	end;
 	char	*str;
 
 	str = 0;
+	if (!s1 && !set)
+		return (NULL);
 	if (s1 != 0 && set != 0)
 	{
-		strat = 0;
+		start = 0;
 		end = ft_strlen(s1);
-		while (s1[strat] && ft_strchr(set, s1[strat]))
-			strat++;
-		while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > strat)
+		while (s1[start] && ft_strchr(set, s1[start]))
+			start++;
+		while (end > start && s1[end - 1] && ft_strchr(set, s1[end - 1]))
 			end--;
-		str = (char *)malloc(sizeof(char) * (end - strat + 1));
+		str = (char *)malloc(end - start + 1);
 		if (str)
-			ft_strlcpy(str, &s1[strat], end - strat + 1);
+			ft_strlcpy(str, &s1[start], end - start + 1);
 	}
 	return (str);
 }
