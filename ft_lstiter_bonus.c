538b4c992a5a4434e8634620cc1f706240dd714a
paromero <paromeropons@hotmail.com>
2023-10-30 12:40:20 +0100

    hecho lstiter

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:34:30 by paromero          #+#    #+#             */
-/*   Updated: 2023/10/27 10:34:32 by paromero         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:35:40 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*tmp;
+
+	tmp = lst;
+	while (tmp->content != NULL)
+	{
+		f(tmp->content);
+	}
+}
